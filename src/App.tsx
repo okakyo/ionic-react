1dde857b28fdb9f5aa4795df87432cde836e094c
OkaKyo <0622okakyo@gmail.com>
2020-08-31 12:07:33 +0900

    FirstCommit

--- src/App.tsx
+++ src/App.tsx
@@ -11,9 +11,9 @@ import {
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import { ellipse, square, triangle } from 'ionicons/icons';
-import Tab1 from './pages/Tab1';
-import Tab2 from './pages/Tab2';
-import Tab3 from './pages/Tab3';
+import Tab1 from './pages/top/TopPage';
+import Tab2 from './pages/about/AboutPage';
+import Tab3 from './pages/setting/SettingPage';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
